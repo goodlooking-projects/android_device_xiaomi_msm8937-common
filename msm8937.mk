4470a6b106dab2d1ae3255234bd951acdd667d68
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-12-09 19:59:24 +0100

    msm8937-common: biometrics: Build libunwind for gx_fpd
    Change-Id: If1d1bc2f9d8cb863b5215e6b981f00c88f4430a4

--- msm8937.mk
+++ msm8937.mk
@@ -133,7 +133,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.land
+    android.hardware.biometrics.fingerprint@2.1-service.land \
+    libunwind
 
 # FM
 PRODUCT_PACKAGES += \
