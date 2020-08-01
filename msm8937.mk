54e64ad4e17beec344662aeae9fc2e5e9b4bb25b
baunilla <49725678+baunilla@users.noreply.github.com>
2020-08-01 22:03:43 +0600

    msm8937-common: livedisplay: Import custom LiveDisplay 2.0 HAL
    Pulled from LineageOS/android_hardware_lineage_livedisplay/sdm
    
    Cleaned up and adapted for PictureAdjustment (vendor) only:
    
    * Removed AdaptiveBacklight, DisplayModes, Constants, Types, Utils
    * Moved necessary constants to anonymous namespace
    
    Change-Id: Ie8421256c4d7a9413b605471299dc7d83f88a4b5

--- msm8937.mk
+++ msm8937.mk
@@ -216,7 +216,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service.xiaomi_land
 
 # Media
 PRODUCT_COPY_FILES += \
