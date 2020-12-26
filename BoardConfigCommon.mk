b21a64835577dac35fd87d140d67dab939a4e841
zeelog <zeelogkang@gmail.com>
2020-12-26 21:10:17 +0700

    msm8937-common: light: Convert lights HAL into a native binderized HAL
    Signed-off-by: sweetyicecare <gabrielseedev@outlook.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -137,9 +137,6 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
 
-# Lights
-TARGET_PROVIDES_LIBLIGHT := true
-
 # Malloc
 MALLOC_SVELTE := true
 
