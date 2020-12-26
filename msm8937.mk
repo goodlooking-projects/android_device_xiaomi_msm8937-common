00e50dda483e69000d31b29303f4574dd029c667
luk1337 <priv.luk@gmail.com>
2020-12-26 23:09:06 +0700

    mam8937-common: Build qti hidl telephony packages
    Signed-off-by: sweetyicecare <gabrielseedev@outlook.com>

--- msm8937.mk
+++ msm8937.mk
@@ -184,7 +184,8 @@ PRODUCT_PACKAGES += \
 
 # IMS
 PRODUCT_PACKAGES += \
-    ims-ext-common
+    ims-ext-common \
+    ims_ext_common.xml
 
 # IRQ balance
 PRODUCT_COPY_FILES += \
@@ -300,6 +301,9 @@ PRODUCT_COPY_FILES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    libcnefeatureconfig \
+    libprotobuf-cpp-full \
+    libprotobuf-cpp-lite \
     librmnetctl \
     libxml2
 
@@ -349,6 +353,10 @@ PRODUCT_COPY_FILES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
