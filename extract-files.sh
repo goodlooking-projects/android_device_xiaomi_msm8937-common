2d535bf2a8996ae58a4030b2b9a126139413f6b7
Gabrielse <gabrielseedev@outlook.com>
2021-01-20 00:43:00 +0700

    msm8937-common: Update ADSP blobs from LA.UM.8.6.2.r1-08100-89xx.0
    - also set device target on shell scripts

--- extract-files.sh
+++ extract-files.sh
@@ -7,6 +7,10 @@
 
 set -e
 
+DEVICE=land
+DEVICE_COMMON=msm8937-common
+VENDOR=xiaomi
+
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -7,6 +7,9 @@
 
 set -e
 
+DEVICE=land
+DEVICE_COMMON=msm8937-common
+VENDOR=xiaomi
 INITIAL_COPYRIGHT_YEAR=2021
 
 # Load extract_utils and do some sanity checks
